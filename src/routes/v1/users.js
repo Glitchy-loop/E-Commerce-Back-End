aebc3da72c581a2ee0db82a8a91fd62e2bb94c16
Audrius <darkinforest@gmail.com>
2022-06-20 22:23:03 +0300

    added POST Register user

--- src/routes/v1/users.js
+++ src/routes/v1/users.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mysql = require('mysql2/promise')
 const { mysqlConfig } = require('../../config')
+const bcrypt = require('bcrypt')
 const router = express.Router()
 
 // Get all users
@@ -8,10 +9,38 @@ router.get('/', async (req, res) => {
   try {
     const connection = await mysql.createConnection(mysqlConfig)
     const [data] = await connection.execute(`
-    SELECT * FROM users
+    SELECT name, email FROM users
     `)
     await connection.end()
     res.status(200).send(data)
+  } catch (err) {
+    return res.status(500).send({ err: 'Server issue. Try again later.' })
+  }
+})
+
+// Register new user
+router.post('/register', async (req, res) => {
+  try {
+    const hash = bcrypt.hashSync(req.body.password, 10)
+
+    const connection = await mysql.createConnection(mysqlConfig)
+    const [data] = await connection.execute(`
+        INSERT INTO users (name, email, password, admin)
+        VALUES (${mysql.escape(req.body.name)}, ${mysql.escape(
+      req.body.email
+    )}, '${hash}', ${mysql.escape(req.body.admin)})
+        `)
+
+    if (!data.insertId || data.affectedRows !== 1) {
+      await connection.end()
+      return res.status(500).send({ err: 'Server issue. Try again later.' })
+    }
+
+    await connection.end()
+    return res.status(200).send({
+      msg: 'Successfully created account',
+      accountId: data.insertId
+    })
   } catch (err) {
     return res.status(500).send({ err: 'Server issue. Try again later.' })
   }
