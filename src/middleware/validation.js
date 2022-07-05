b1a7b6ba7f1e0488b56f170c731b577278377e00
Audrius <darkinforest@gmail.com>
2022-07-05 17:33:14 +0300

    updated add product

--- src/middleware/validation.js
+++ src/middleware/validation.js
@@ -1,6 +1,7 @@
 const validation = schema => async (req, res, next) => {
   try {
     req.body = await schema.validateAsync(req.body)
+    console.log(req.body)
     return next()
   } catch (err) {
     console.log(err)
