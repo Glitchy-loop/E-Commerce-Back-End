926f5e795873f74c64eb139cd17fa7f9fe8ed0e2
Audrius <darkinforest@gmail.com>
2022-07-07 00:16:20 +0300

    master

--- src/routes/v1/products.js
+++ src/routes/v1/products.js
@@ -57,13 +57,13 @@ router.post(
   upload.array('img'),
   async (req, res) => {
     try {
-      const results = await s3Upload(req.files)
+      await s3Upload(req.files)
       // console.log(req.files)
       const connection = await mysql.createConnection(mysqlConfig)
       const [data] = await connection.execute(`
       INSERT INTO products (img, title, category, price, description, inStock, archived)
       VALUES (
-        '${results[0].key.split('/').pop()}', ${mysql.escape(
+        '${req.files.originalname}', ${mysql.escape(
         req.body.title
       )},${mysql.escape(req.body.category)},${mysql.escape(
         req.body.price
